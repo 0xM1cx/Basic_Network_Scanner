4ab471dfa8d16385fbf997160027084e946c6e2c
Shawn Sudaria <shawnlovecode14@gmail.com>
2022-05-20 15:00:18 +0800

    Update

--- main.py
+++ main.py
@@ -3,22 +3,45 @@ import re
 import subprocess
 from time import sleep
 from colorama import Fore
+from scapy.all import Ether, srp, ARP
+
+
+
 
 parser = argparse.ArgumentParser() # Initializes argparse
 
-IP_Help = "Destination IP Address. E.g. 192.168.1.1"
+IP_Help = "Destination IP Address. E.g. 192.168.1.1 or 192.168.1.0/24"
 
 parser.add_argument("-ip", type=str, required=True, help=IP_Help) # adds -ip arguments and sets it data type to string and set to mandatory
 args = parser.parse_args()
 
-def scan(ip):
-    # with open("log.txt", "a") as f:
-    #     send = subprocess.check_output(["ping", "-c", "5", ip]).decode("utf-8")
-    #     f.write(send) # Writes the ICMP echo reply to a log.txt file.
-    subprocess.check_output(["ping", "-c", "5", ip]).decode("utf-8")
+def scanSpecificAdd(ip):
+    arp = ARP(pdst=ip) # Creating an ARP Packet
+    ether = Ether(dst="ff:ff:ff:ff:ff:ff") # Creating a broadcast Packet
+    packet = ether/arp # Stacking the arp and ether Packets.
+
+    result = srp(packet, timeout=3)[0]
+    print(packet)
     
+def scanSubnetAdd(ip):
+    # # for i in range
+    # # subprocess.check_output(["ping", "-c", "2", ip])
+    # ipArr = re.split(r"[.|/]", ip)
+    # ip = ".".join(ipArr[0:4])
+
+    arp = ARP(pdst=ip) # Creating an ARP Packet
+    ether = Ether(dst="ff:ff:ff:ff:ff:ff") # Creating a broadcast Packet
+    packet = ether/arp # Stacking the arp and ether Packets.
+
+    result = srp(packet, timeout=3)[0]
 
 
+    clients = []
+
+    for sent, received in result:
+        clients.append({'ip': received.psrc, 'mac': received.hwsrc})
+    
+    print(clients)
 
 
 ListedAddress = re.split(r"[.|/]", args.ip) # Splits the ip address into 4 or 5 elements in a list.
@@ -31,8 +54,12 @@ for ip in ListedAddress:
         sleep(5)
         exit()
 
-try:
-    scan(args.ip) # if the input is in IP address format they it will start scanning it.
-    print(Fore.CYAN + "ONLINE" + Fore.WHITE)
-except:
-    print(Fore.RED + "OFFLINE" + Fore.WHITE)
+scanSpecificAdd(args.ip)
+# try:
+#     if len(ListedAddress) == 4:
+#         scanSpecificAdd(args.ip) # if the input is in IP address format they it will start scanning it.
+#     elif len(ListedAddress) == 5: 
+#         scanSubnetAdd(args.ip)  
+#     print(Fore.CYAN + "ONLINE" + Fore.WHITE)
+# except:
+#     print(Fore.RED + "OFFLINE" + Fore.WHITE)
